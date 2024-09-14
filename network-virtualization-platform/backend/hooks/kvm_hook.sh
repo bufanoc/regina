cc55ad4cc1ddd0d11c7ec1af8912f9be75c006ca
Jigawatt <4837231+bufanoc@users.noreply.github.com>
2024-09-13 21:22:48 -0400

    Update kvm_hook.sh
    Libvirt Hook for KVM Event Notification

--- network-virtualization-platform/backend/hooks/kvm_hook.sh
+++ network-virtualization-platform/backend/hooks/kvm_hook.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
+GUEST_NAME=$1
+EVENT=$2
+
+if [ "$EVENT" == "start" ]; then
+    # Fetch VM MAC address using virsh
+    MAC_ADDR=$(virsh dumpxml "$GUEST_NAME" | grep "mac address" | sed "s/.*'\(.*\)'.*/\1/")
+    
+    # Send details to web interface for network binding
+    curl -X POST http://<web-interface-ip>:<web-interface-port>/vm_event \
+         -H "Content-Type: application/json" \
+         -d "{\"vm_name\":\"$GUEST_NAME\", \"mac_address\":\"$MAC_ADDR\"}"
+fi
 
