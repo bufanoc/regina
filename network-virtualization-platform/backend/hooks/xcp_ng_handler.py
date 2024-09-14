72049a16886dadea27f03a151705492f636feb09
Jigawatt <4837231+bufanoc@users.noreply.github.com>
2024-09-13 21:24:08 -0400

    Update xcp_ng_handler.py
    XAPI Event Handler for XCP-NG

--- network-virtualization-platform/backend/hooks/xcp_ng_handler.py
+++ network-virtualization-platform/backend/hooks/xcp_ng_handler.py
@@ -1,1 +1,20 @@
+from XenAPI import Session
+import requests
+
+# XCP-NG/XenServer API connection
+session = Session('http://xcp-ng-ip')
+session.xenapi.login_with_password('username', 'password')
+
+# Fetch VM details and send to web interface
+def on_vm_event(event):
+    if event['operation'] == 'add' or event['operation'] == 'mod':
+        vm_uuid = event['ref']
+        mac_address = session.xenapi.VM.get_MAC(vm_uuid)
+        vm_name = session.xenapi.VM.get_name_label(vm_uuid)
+
+        # Send VM details to web interface
+        requests.post("http://<web-interface-ip>:<web-interface-port>/vm_event", json={
+            "vm_name": vm_name,
+            "mac_address": mac_address
+        })
 
